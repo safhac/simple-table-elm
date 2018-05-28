5a68f2f71f6627e594266de51c8a08dfdfc839fb
Asaf Cohen <safhac@gmail.com>
2018-05-28 15:11:21 +0200

    initial commit

---
+++ src/elm/Main.elm
@@ -0,0 +1,163 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, table, td, text, th, thead, tr)
+import Html.Events exposing (onClick)
+import Styles exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    { list : List Note
+    , sorter : Msg
+    }
+
+
+type alias Note =
+    { body : String
+    , dateCreated : Float
+    }
+
+
+
+-- Msg
+
+
+type Msg
+    = ByText Bool
+    | ByDate Bool
+
+
+
+-- Init
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { list = initialNotes
+      , sorter = ByText True
+      }
+    , Cmd.none
+    )
+
+
+createNote : String -> Note
+createNote message =
+    { body = toString message
+    , dateCreated = 0
+    }
+
+
+initialNotes : List Note
+initialNotes =
+    [ createNote "first"
+    , createNote "wat?"
+    , createNote "hi"
+    , createNote "lol"
+    ]
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ByDate flag ->
+            let
+                sorted =
+                    if flag == True then
+                        model.list
+                            |> List.sortBy .dateCreated
+                    else
+                        model.list
+                            |> List.sortBy .dateCreated
+                            |> List.reverse
+            in
+            { list = sorted, sorter = ByDate flag } ! []
+
+        ByText flag ->
+            let
+                sorted =
+                    if flag == True then
+                        model.list
+                            |> List.sortBy .body
+                    else
+                        model.list
+                            |> List.sortBy .body
+                            |> List.reverse
+            in
+            { list = sorted, sorter = ByText flag } ! []
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        header =
+            renderHead model.sorter
+
+        body =
+            renderRows model.list
+    in
+    div [ standardContainerStyle ]
+        [ table [ tableStyle ]
+            (header
+                :: body
+            )
+        ]
+
+
+renderHead : Msg -> Html Msg
+renderHead msg =
+    let
+        dateFlag =
+            case msg of
+                ByDate True ->
+                    False
+
+                _ ->
+                    True
+
+        textFlag =
+            case msg of
+                ByText True ->
+                    False
+
+                _ ->
+                    True
+    in
+    thead []
+        [ th [ onClick (ByText textFlag) ] [ text "note" ]
+        , th [ onClick (ByDate dateFlag) ] [ text "date" ]
+        ]
+
+
+renderRows : List Note -> List (Html Msg)
+renderRows list =
+    list
+        |> List.map renderNote
+
+
+renderNote : Note -> Html msg
+renderNote { body, dateCreated } =
+    tr [ rowStyle ]
+        [ td [] [ text body ]
+        , td [] [ toString dateCreated |> text ]
+        ]
---
+++ src/elm/Styles.elm
@@ -0,0 +1,101 @@
+module Styles exposing (..)
+
+import Html exposing (Attribute)
+import Html.Attributes exposing (class, id, style, type_)
+
+
+standardContainerStyle : Attribute msg
+standardContainerStyle =
+    style
+        [ ( "display", "flex" )
+        , ( "text-align", "center" )
+        , ( "flex", "1" )
+        , ( "flex-direction", "column" )
+        , ( "font-family", "sans-serif" )
+        ]
+
+
+tableStyle : Attribute msg
+tableStyle =
+    style
+        [ ( "background-color", "lightgray" )
+        , ( "border-radius", "3px" )
+        , ( "margin", "auto" )
+        , ( "width", "50vw" )
+        ]
+
+
+rowStyle : Attribute msg
+rowStyle =
+    style
+        [ ( "line-height", "50px" )
+        , ( "color", "#3c5966" )
+        ]
+
+
+selected : Attribute msg
+selected =
+    style
+        [ ( "background-color", "rgb(107, 107, 107)" )
+        , ( "color", "rgb(207, 207, 207)" )
+        , ( "border", "1px solid #888" )
+        , ( "box-shadow", "0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19)" )
+        ]
+
+
+edited : Attribute msg
+edited =
+    style
+        [ ( "width", "100%" )
+        , ( "height", "45px" )
+        , ( "border-radius", "3px" )
+        , ( "border", "none" )
+        , ( "text-align", "center" )
+        , ( "font-size", "16px" )
+        ]
+
+
+deleted : Attribute msg
+deleted =
+    style
+        [ ( "text-shadow", "white 0px 1px 0px" )
+        , ( "border", "1px solid rgba(255, 255, 255, 0.6)" )
+        , ( "color", "rgba(0, 0, 0, 0.4)" )
+        ]
+
+
+created : Attribute msg
+created =
+    style [ ( "color", "rgba(0, 0, 0, 0.6)" ) ]
+
+
+displayed : Attribute msg
+displayed =
+    style [ ( "box-shadow", "0 1px 1px 0 rgba(0,0,0,0.2),0 1px 1px 0 rgba(0,0,0,0.19)" ) ]
+
+
+hidden : Attribute msg
+hidden =
+    style
+        [ ( "background-color", "#fefefe" )
+        , ( "opacity", "0.4" )
+        , ( "color", "transparent" )
+        , ( "text-shadow", "0 0 1px #000 " )
+        ]
+
+
+tHeader : Attribute msg
+tHeader =
+    style
+        [ ( "color", "lightslategrey" )
+        , ( "line-height", "40px" )
+        , ( "cursor", "pointer" )
+        ]
+
+
+searchHeader : Attribute msg
+searchHeader =
+    style
+        [ ( "justify-content", "space-around" )
+        , ( "display", "flex" )
+        ]
